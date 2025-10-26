01f3a0c5a14c27e7e99aca64f6e6a5a6192df14c
JoseMGT-31 <beckansg31@gmail.com>
2025-10-26 19:47:35 -0300

    Se agrego navegacion entre screens
    # Conflicts:
    #	app/src/main/java/com/example/rideon/MainActivity.kt

--- app/src/main/java/com/example/rideon/MainActivity.kt
+++ app/src/main/java/com/example/rideon/MainActivity.kt
@@ -3,36 +3,50 @@ package com.example.rideon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.*
-import com.example.rideon.data.Catalogo
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.rideon.ui.theme.RideOnTheme
-import com.example.rideon.view.screens.CatalogoScreen
-import com.example.rideon.view.screens.ProductoScreen
-import com.example.rideon.view.screens.ProductoUi
+import com.example.rideon.view.screens.HomeAuthScreen
+import com.example.rideon.view.screens.LoginScreen
+import com.example.rideon.view.screens.RegisterScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             RideOnTheme {
-                // estado simple: null = estoy en catálogo; !=null = estoy en detalle
-                var seleccionado by remember { mutableStateOf<ProductoUi?>(null) }
-
-                if (seleccionado == null) {
-                    CatalogoScreen(
-                        productos = Catalogo.productos,
-                        onOpen = { p -> seleccionado = p } // abrir detalle
-                    )
-                } else {
-                    ProductoScreen(
-                        producto = seleccionado!!,
-                        onBack = { seleccionado = null },      // volver al catálogo
-                        onAddToCart = { qty ->
-                            // por ahora solo mostramos en consola
-                            println("Agregaste $qty ${seleccionado!!.model} al carrito")
-                        }
-                    )
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "home"
+                ) {
+                    composable("home") {
+                        HomeAuthScreen(
+                            onLogin = {
+                                navController.navigate("login")
+                            },
+                            onRegister = {
+                                navController.navigate("register")
+                            }
+                        )
+                    }
+                    composable("login") {
+                        LoginScreen(
+                            onLoggedIn = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
+                    composable("register") {
+                        RegisterScreen(
+                            onRegistered = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
                 }
             }
         }
