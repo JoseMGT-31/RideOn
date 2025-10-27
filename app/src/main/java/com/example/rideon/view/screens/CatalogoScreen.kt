37ea58500c21862c3a4e2aeb6fc8c5be6fc71a38
CireyRari <cire.ramirez@duocuc.cl>
2025-10-26 21:09:28 -0300

    feat: Importar ProductoUi
    Se importa el modelo `ProductoUi` en `CatalogoScreen` para su uso futuro en la pantalla del catálogo.

--- app/src/main/java/com/example/rideon/view/screens/CatalogoScreen.kt
+++ app/src/main/java/com/example/rideon/view/screens/CatalogoScreen.kt
@@ -1,5 +1,6 @@
 package com.example.rideon.view.screens
 
+import com.example.rideon.model.ProductoUi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
